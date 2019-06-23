53d23944b699d5cce0e71bebd8abeb51a053216b
DWKE <lkeun1119@gmail.com>
2019-06-23 15:47:21 +0900

    build

--- build/catkin_generated/generate_cached_setup.py
+++ build/catkin_generated/generate_cached_setup.py
@@ -12,16 +12,16 @@ try:
     from catkin.environment_cache import generate_environment_script
 except ImportError:
     # search for catkin package in all workspaces and prepend to path
-    for workspace in "/home/skros/loc_ws/devel;/home/skros/ws_vision/devel;/home/skros/ws_gps/devel;/home/skros/ws_yeongsoo/devel;/home/skros/catkin_ws/devel;/home/skros/ws_ros_can_sangkwon/devel;/opt/ros/kinetic".split(';'):
+    for workspace in "/home/lke/catkin_ws/devel;/opt/ros/kinetic".split(';'):
         python_path = os.path.join(workspace, 'lib/python2.7/dist-packages')
         if os.path.isdir(os.path.join(python_path, 'catkin')):
             sys.path.insert(0, python_path)
             break
     from catkin.environment_cache import generate_environment_script
 
-code = generate_environment_script('/home/skros/Autonomous-car-lab-KUSV/devel/env.sh')
+code = generate_environment_script('/home/lke/ACL_KUSV/devel/env.sh')
 
-output_filename = '/home/skros/Autonomous-car-lab-KUSV/build/catkin_generated/setup_cached.sh'
+output_filename = '/home/lke/ACL_KUSV/build/catkin_generated/setup_cached.sh'
 with open(output_filename, 'w') as f:
     #print('Generate script for cached setup "%s"' % output_filename)
     f.write('\n'.join(code))
--- devel/share/ros_can/cmake/ros_canConfig.cmake
+++ devel/share/ros_can/cmake/ros_canConfig.cmake
@@ -67,14 +67,14 @@ set(ros_can_CONFIG_INCLUDED TRUE)
 
 # set variables for source/devel/install prefixes
 if("TRUE" STREQUAL "TRUE")
-  set(ros_can_SOURCE_PREFIX /home/skros/Autonomous-car-lab-KUSV/src/sensing/ros_can)
-  set(ros_can_DEVEL_PREFIX /home/skros/Autonomous-car-lab-KUSV/devel)
+  set(ros_can_SOURCE_PREFIX /home/lke/ACL_KUSV/src/sensing/ros_can)
+  set(ros_can_DEVEL_PREFIX /home/lke/ACL_KUSV/devel)
   set(ros_can_INSTALL_PREFIX "")
   set(ros_can_PREFIX ${ros_can_DEVEL_PREFIX})
 else()
   set(ros_can_SOURCE_PREFIX "")
   set(ros_can_DEVEL_PREFIX "")
-  set(ros_can_INSTALL_PREFIX /home/skros/Autonomous-car-lab-KUSV/install)
+  set(ros_can_INSTALL_PREFIX /home/lke/ACL_KUSV/install)
   set(ros_can_PREFIX ${ros_can_INSTALL_PREFIX})
 endif()
 
@@ -91,9 +91,9 @@ endif()
 # flag project as catkin-based to distinguish if a find_package()-ed project is a catkin project
 set(ros_can_FOUND_CATKIN_PROJECT TRUE)
 
-if(NOT "/home/skros/Autonomous-car-lab-KUSV/src/sensing/ros_can/include " STREQUAL " ")
+if(NOT "/home/lke/ACL_KUSV/src/sensing/ros_can/include " STREQUAL " ")
   set(ros_can_INCLUDE_DIRS "")
-  set(_include_dirs "/home/skros/Autonomous-car-lab-KUSV/src/sensing/ros_can/include")
+  set(_include_dirs "/home/lke/ACL_KUSV/src/sensing/ros_can/include")
   if(NOT " " STREQUAL " ")
     set(_report "Check the issue tracker '' and consider creating a ticket if the problem has not been reported yet.")
   elseif(NOT " " STREQUAL " ")
@@ -110,7 +110,7 @@ if(NOT "/home/skros/Autonomous-car-lab-KUSV/src/sensing/ros_can/include " STREQU
         message(FATAL_ERROR "Project 'ros_can' specifies '${idir}' as an include dir, which is not found.  It does not exist in '${include}'.  ${_report}")
       endif()
     else()
-      message(FATAL_ERROR "Project 'ros_can' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/skros/Autonomous-car-lab-KUSV/src/sensing/ros_can/${idir}'.  ${_report}")
+      message(FATAL_ERROR "Project 'ros_can' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/lke/ACL_KUSV/src/sensing/ros_can/${idir}'.  ${_report}")
     endif()
     _list_append_unique(ros_can_INCLUDE_DIRS ${include})
   endforeach()
@@ -129,7 +129,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/skros/Autonomous-car-lab-KUSV/devel/lib;/home/skros/loc_ws/devel/lib;/home/skros/ws_vision/devel/lib;/home/skros/ws_gps/devel/lib;/home/skros/ws_yeongsoo/devel/lib;/home/skros/catkin_ws/devel/lib;/home/skros/ws_ros_can_sangkwon/devel/lib;/opt/ros/kinetic/lib)
+    foreach(path /home/lke/ACL_KUSV/devel/lib;/home/lke/catkin_ws/devel/lib;/opt/ros/kinetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
