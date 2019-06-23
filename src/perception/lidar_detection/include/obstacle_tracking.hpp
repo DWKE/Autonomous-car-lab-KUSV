7d2e741616f0c84e8c4594bf5c1a3156f22c4a45
kusvcom2 <lkeun1119@gmail.com>
2019-06-24 02:01:25 +0900

    Lidar update

---
+++ src/perception/lidar_detection/include/obstacle_tracking.hpp
@@ -0,0 +1,32 @@
+#ifndef __OBSTACLE_TRACKING_H__
+#define __OBSTACLE_TRACKING_H__
+
+#include <ros/ros.h>
+#include <vector>
+#include <iostream>
+#include <math.h>
+#include <pcl_ros/point_cloud.h>
+
+#include "cluster.hpp"
+
+
+class ObstacleTracking
+{
+	public:
+	std::vector<clusterPtr> m_DetectedObjects;
+	std::vector<clusterPtr> m_TrackingObjects;
+	std::vector<clusterPtr> m_TmpTrackingObjects;
+	double m_MAX_ASSOCIATION_DISTANCE;
+	unsigned int iTracksNumber;
+
+	public:
+	ObstacleTracking();
+	~ObstacleTracking();
+	//void doOneStep (const std::vector<clusterPtr>& objList);
+	void associate (const std::vector<clusterPtr>& objList);
+	void matchWithDistanceOnly ();
+	void MergeObjectAndTrack (clusterPtr& track, clusterPtr& obj);
+};
+
+
+#endif //__OBSTACLE_TRACKING_H__
