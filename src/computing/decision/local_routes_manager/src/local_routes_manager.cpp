11490378816033c55a06b1f34119515cab5423c0
DWKE <lkeun1119@gmail.com>
2019-06-24 12:27:22 +0900

    Modify local_routes_manager

--- src/computing/decision/local_routes_manager/src/local_routes_manager.cpp
+++ src/computing/decision/local_routes_manager/src/local_routes_manager.cpp
@@ -31,8 +31,10 @@ private:
 
     geometry_msgs::PoseStamped curr_pose;
     geometry_msgs::Pose change_cp;
-    geometry_msgs::Pose tmp_change_cp;
+    geometry_msgs::Pose tmp_change_cp[4];
+    geometry_msgs::Pose slow_cp;
     float range_rad;
+    int tmp_change_cnt=0;
 
 public:
     LocalRoutesManager()
@@ -46,17 +48,31 @@ public:
         mode_change_flag=false;
         tmp_mode_change_flag=false;
 
-        //x: 14146112.5561
-        //y: 4151255.99135
-        change_cp.position.x = 74.3688746371;
-        change_cp.position.y = 119.578274418;
+
+        change_cp.position.x = 92.0;
+        change_cp.position.y = 166.5;
         change_cp.position.z = 0;
 
-        tmp_change_cp.position.x = 46.1064110262;
-        tmp_change_cp.position.y = 54.6836921825;
-        tmp_change_cp.position.z = 0;
+        tmp_change_cp[0].position.x = 46.1064110262;
+        tmp_change_cp[0].position.y = 54.6836921825;
+        tmp_change_cp[0].position.z = 0;
 
-        range_rad = 5;
+        tmp_change_cp[1].position.x = 74.3688746371;
+        tmp_change_cp[1].position.y = 119.578274418;
+        tmp_change_cp[1].position.z = 0;
+
+        tmp_change_cp[2].position.x = 50.3219096485;
+        tmp_change_cp[2].position.y = 70.5660489841;
+        tmp_change_cp[2].position.z = 0;
+
+        tmp_change_cp[3].position.x = 23.1107076412;
+        tmp_change_cp[3].position.y = 34.2175396228;
+        tmp_change_cp[3].position.z = 0;
+
+        slow_cp.position.x = 104.707503191;
+        slow_cp.position.y = 202.452903301;
+
+        range_rad = 10;
     }
 
     void laneCallback(const kusv_msgs::PolyfitLaneData::ConstPtr& lane) {
@@ -75,24 +91,26 @@ public:
 
     void modeChange() {
         float distance = sqrt(pow(change_cp.position.x - curr_pose.pose.position.x,2) + pow(change_cp.position.y - curr_pose.pose.position.y,2));
-        if(!mode_change_flag && distance<range_rad) {
+        if(!mode_change_flag && distance<range_rad*2) {
             driving_mode = !driving_mode;
             mode_change_flag = true;
             ROS_INFO("mode changed to %d",driving_mode);
-        }else if(mode_change_flag && distance>range_rad) {
+        }
+        else if(mode_change_flag && distance>range_rad) {
             mode_change_flag = false;
-            ROS_INFO("mode changed to %d",driving_mode);
         }
 
-        float tmp_distance = sqrt(pow(tmp_change_cp.position.x - curr_pose.pose.position.x,2) + pow(tmp_change_cp.position.y - curr_pose.pose.position.y,2));
+        float tmp_distance = sqrt(pow(tmp_change_cp[tmp_change_cnt].position.x - curr_pose.pose.position.x,2) + pow(tmp_change_cp[tmp_change_cnt].position.y - curr_pose.pose.position.y,2));
         if(!tmp_mode_change_flag && tmp_distance<range_rad) {
             driving_mode = !driving_mode;
             tmp_mode_change_flag = true;
             ROS_INFO("mode changed to %d",driving_mode);
+
         }else if(tmp_mode_change_flag && tmp_distance>range_rad) {
             driving_mode = !driving_mode;
             tmp_mode_change_flag = false;
             ROS_INFO("mode changed to %d",driving_mode);
+            tmp_change_cnt++;
         }
     }
 
